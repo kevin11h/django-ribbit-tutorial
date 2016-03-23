937d62668e7be282a8424bd0d43a02d54535c368
kevin11h <kevhung11@gmail.com>
2016-03-23 15:20:33 -0700

    Implemented User Profiles and Following Relation on frontend

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -28,6 +28,9 @@ urlpatterns = patterns('',
     url(r'^login$', 'ribbit_app.views.login_view'), # lgoin
     url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
     url(r'^signup$', 'ribbit_app.views.signup'), # signup
-    url(r'^ribbit$', 'ribbit_app.views.public'),
-    url(r'^submit$', 'ribbit_app.views.submit')
+    url(r'^ribbits$', 'ribbit_app.views.public'),
+    url(r'^submit$', 'ribbit_app.views.submit'),
+    url(r'^users/$', 'ribbit_app.views.users'),
+    url(r'^users/(?P<username>\w{0,30})/$', 'ribbit_app.views.users'),
+    url(r'^follow$', 'ribbit_app.views.follow')
 )
